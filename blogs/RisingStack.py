50fd7bbfcc1d4cfa632b29d73c6999009ffee4be
Chris Wan <wpy.christopher@gmail.com>
2020-10-02 12:32:03 +0800

    Add blog source

---
+++ blogs/RisingStack.py
@@ -0,0 +1,26 @@
+import time
+import requests
+
+from bs4 import BeautifulSoup
+from utils import sendmail
+
+
+class RisingStack():
+	ORIGIN = "https://blog.risingstack.com"
+
+	@classmethod
+	def get(cls):
+		r = requests.get(cls.ORIGIN)
+		soup = BeautifulSoup(r.text, "html.parser")
+		for post in soup.find_all("article"):
+			header = post.find("header", attrs={ "class": "post-header" })
+			a = header.find("a")
+			subject = f'[RisingStack] {a.get_text()}'
+			link = f'{cls.ORIGIN}{a.get("href")}'
+			section = post.find("section", attrs={ "class": "post-content" })
+			content = f'''
+			{section.get_text()}
+			{link}
+			'''
+			sendmail(subject, content)
+			time.sleep(3)
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import tornado.ioloop
 import tornado.web
 
+import blogs
+
+TWENTY_MINS_IN_MILLISECONDS = 20 * 60 * 1000
+
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Hello, world")
@@ -10,7 +14,13 @@ def make_app():
 		(r"/", MainHandler),
 	])
 
+def digesting():
+	blogs.RisingStack.get()
+
 if __name__ == "__main__":
 	app = make_app()
 	app.listen(8888)
+	digesting()
+	scheduler = tornado.ioloop.PeriodicCallback(digesting, TWENTY_MINS_IN_MILLISECONDS)
+	scheduler.start()
 	tornado.ioloop.IOLoop.current().start()
